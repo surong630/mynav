665dd9538ef2d568b73c0aeb900c67c86e3eb593
insistsu <947737976@qq.com>
2021-01-13 23:37:04 +0800

    ok

--- src/js/main.js
+++ src/js/main.js
@@ -5,23 +5,39 @@ let x = localStorage.getItem('x')
 let xObject = JSON.parse(x);
 // 动态创建数组，然后通过动态添加和删除。 判断localStorage有没有存在数据,有的话则用,没有就用默认的
 const hashMap = xObject ||  [
-  {logo: 'A', logoType: 'text', url: 'https://www.acfun.cn'},
-  {logo: './images/blibli.png', logoType: 'image', url: 'https://www.bilibibli.com'}
+  {logo: 'A', url: 'https://www.acfun.cn'},
+  {logo: 'B', url: 'https://www.bilibibli.com'}
 ]
+const simplifyUrl = (url) => {
+  let newUrl = url.replace('https://', '').replace('http://', '').replace('www.', '').replace(/\/.*/, '')
+  return newUrl
+}
 const render = () => {
-// 遍历数组，将每一项插入到新增网站之前
-  hashMap.forEach(node => {
+  // 找到除了最后一个的li然后每个都移除掉
+    $siteList.find('li:not(.last)').remove()
+  // 遍历数组，将每一项插入到新增网站之前
+  hashMap.forEach((node, index) => {
     const $li = $(`<li>
-    <a href="${node.url}">
+    <a href="${node.url}" target="_blank">
       <div class="site">
         <div class="logo">
-          ${node.logo[0]}
+          ${simplifyUrl(node.url)[0].toUpperCase()}
         </div>
-        <div class="link">${node.url}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+          <div class="close">
+            <svg class="icon">
+              <use xlink:href="#icon-close"></use>
+            </svg>
+          </div>
       </div>
     </a>
   </li>
     `).insertBefore($last)
+    $li.on('click', '.close', e => {
+      e.preventDefault();
+      hashMap.splice(index, 1)
+      render()
+    })
   })
 }
 render()
@@ -33,13 +49,21 @@ $('.addButton').on('click', () => {
     url = 'https://' + url
   }
   // 将新添加的url加入到hashMap数组中
-  hashMap.push({logo: url, logoType: 'text', url: url});
-  // 找到除了最后一个的li然后每个都移除掉
-  $siteList.find('li:not(.last)').remove()
+  hashMap.push({logo: url[8], url: url});
+
   // 再生成新的样式
   render()
 })
+
 window.onbeforeunload = () => {
   let string = JSON.stringify(hashMap);
   localStorage.setItem('x', string)
 }
+$(document).on('keypress', e => {
+  const { key } = e;
+  for(let i=0;i< hashMap.length;i++) {
+    if(hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url)
+    }
+  }
+})
